6c304ff71b72fc2ff0d3f65cea5f3ec8c50f3d19
Jay Kim <jkim2@grubhub.com>
2021-02-25 22:38:38 -0600

    0.38

--- setup.py
+++ setup.py
@@ -18,7 +18,9 @@ setup(
 	url="https://github.com/mozjay0619/pyflow-viz",
 	license="DSB 3-clause",
 	packages=find_packages(),
-	install_requires=["graphviz>=0.13.2", "bokeh>=2.0.1", "scipy>=1.4.1", 
-					  "scikit-image>=0.17.2", "numpy>=1.18.2", "pandas>=0.25.3",
-					  "ipython"]
+	# install_requires=["graphviz>=0.13.2", "bokeh>=2.0.1", "scipy>=1.4.1", 
+	# 				  "scikit-image>=0.17.2", "numpy>=1.18.2", "pandas>=0.25.3",
+	# 				  "ipython"]
+	install_requires=["scipy>=1.4.1", 
+					  "numpy>=1.18.2", "pandas>=0.25.3"]
 	)
